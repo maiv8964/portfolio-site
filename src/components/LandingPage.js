059d042c5d246d4cbcfa419e1a433faee99c483d
Vincent Mai <maiv8964@my.yorku.ca>
2025-07-21 02:00:43 -0400

    initial work

---
+++ src/components/LandingPage.js
@@ -0,0 +1,27 @@
+import { HStack, VStack, Stack, Heading, Image, Center } from "@chakra-ui/react"
+import babyskipper from '../images/babyskipper.jpg'
+const LandingPage = () => {
+    return (
+
+        <Center height="100vh">
+            <HStack gap="10">
+                <Stack>
+                    <VStack>
+                        <Heading size="6xl">Hi, I'm Vincent</Heading>
+                        <Heading size="6xl">Software Engineer</Heading>
+                    </VStack>
+                </Stack>
+                <Image 
+                    src={babyskipper}
+                    boxSize="150px"
+                    borderRadius="full"
+                    fit="cover"
+                    alt="Vincent Mai"
+                />
+            </HStack>
+        </Center>
+        
+    )
+}
+
+export default LandingPage
